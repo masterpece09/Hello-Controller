6d6b82d5ed91cd115153279ae16f8db46a3fbd9f
ELOUNDOU Célestin Placide <masterpece09@gmail.com>
2022-04-21 05:12:25 +0100

    ADD Uni test

--- src/test/java/com/example/demo/HelloDockerApplicationTests.java
+++ src/test/java/com/example/demo/HelloDockerApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
 class HelloDockerApplicationTests {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	public void testMockMVC() throws Exception {
+		MvcResult result = mockMvc.perform(get("/hello").contentType("application/json")).andExpect(status().isOk()).andReturn();
+		assertThat(result.getResponse().getContentAsString()).isEqualTo("Hello World from Docker!");
+		
+	}
+	
+	
 
 }
