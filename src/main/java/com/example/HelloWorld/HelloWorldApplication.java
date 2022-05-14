242416f0ca105f03a06541569b21388f899c1b95
ELOUNDOU Célestin Placide <masterpece09@gmail.com>
2022-05-14 12:36:48 +0100

    MAMA ELOUNDOU

--- src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -25,6 +25,6 @@ public class HelloWorldApplication extends SpringBootServletInitializer
     @RequestMapping(value = "/hello")
 	public String helloWorld()
 	{
-		return "Hello World, ADO LA MAMA Des ELOUNDOU";
+		return "Hello World, ADO LA MAMA Des ELOUNDOU YES";
 	}
 }
